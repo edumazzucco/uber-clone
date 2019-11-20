23633176215fb191b97296363a63e82f7c28c928
edumazzucco <eduardo@wized.com.br>
2019-11-20 20:37:35 -0300

    first commit, interface almost done, need to customize the ride info details and fix route bug

---
+++ src/components/Details/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #FFF;
+  height: 300px;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  shadow-color: #000;
+  shadow-offset: 0 0;
+  shadow-opacity: 0.2;
+  shadow-radius: 10;
+  elevation: 3;
+  border: 1px solid #DDD;
+  align-items: center;
+  padding: 20px;
+`;
+
+export const TypeTitle = styled.Text`
+  
+`;
+
+export const TypeDescription = styled.Text`
+  
+`;
+
+export const TypeImage = styled.View`
+  
+`;
+
+export const RequestButton = styled.TouchableOpacity`
+  
+`;
+
+export const RequestButtonText = styled.Text`
+  
+`;
